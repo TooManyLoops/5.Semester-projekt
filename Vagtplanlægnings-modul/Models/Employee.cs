7c5c364f6f0bf9ca3a516d4757446f7dd53eb350
frittemand <150781905+frittemand@users.noreply.github.com>
2026-05-06 12:56:57 +0200

    USER - 01 - Done

--- Vagtplanlægnings-modul/Models/Employee.cs
+++ Vagtplanlægnings-modul/Models/Employee.cs
@@ -10,29 +10,21 @@ public class Employee
     [Column("employee_Id")]
     public Guid EmployeeId { get; set; }
 
-    [Required]
     [Column("firstName")]
     public string FirstName { get; set; } = null!;
 
-    [Required]
     [Column("lastName")]
     public string LastName { get; set; } = null!;
 
-    [Required]
-    [EmailAddress]
     [Column("email")]
     public string Email { get; set; } = null!;
 
-    [Required]
-    [Phone]
     [Column("phoneNr")]
     public string PhoneNumber { get; set; } = null!;
 
-    [Required]
     [Column("employeeStatus")]
     public EmployeeStatus Status { get; set; }
 
-    [Required]
     [DataType(DataType.Date)]
     [Column("hiredAt")]
     public DateOnly HiredAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
