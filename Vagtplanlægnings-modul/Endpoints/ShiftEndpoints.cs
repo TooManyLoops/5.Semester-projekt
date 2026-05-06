03411eaf7a02ca6a79ed2f605b904314b5858309
JepperV2 <10404844@ucn.dk>
2026-05-06 14:26:22 +0200

    Add CreateShift endpoint for /Shift
    Introduce MapUserEndpoint in ShiftEndpoints to register a POST /Shift route named CreateShift. The new CreateShift handler calls EmployeeService.CreateEmployee with an EmployeeRequest and returns 201 Created with Location set to /Employees/{EmployeeId}. Also apply a minor formatting change to the Microsoft.VisualStudio.Azure.Containers.Tools.Targets PackageReference in the project file.

--- Vagtplanlægnings-modul/Endpoints/ShiftEndpoints.cs
+++ Vagtplanlægnings-modul/Endpoints/ShiftEndpoints.cs
@@ -6,5 +6,22 @@ namespace Vagtplanlægnings_modul.Endpoints
 {
     public static class ShiftEndpoints
     {
+        public static WebApplication MapUserEndpoint(this WebApplication app)
+        {
+            var userEndpoint = app.MapGroup("/Shift");
+            userEndpoint.MapPost("/", CreateShift).WithName("CreateShift");
+
+            return app;
+        }
+
+        public static async Task<IResult> CreateShift(
+            EmployeeService service,
+            EmployeeRequest request
+        )
+        {
+            var result = await service.CreateEmployee(request);
+            return TypedResults.Created($"/Employees/{result.EmployeeId}", result);
+        }
+
     }
 }
