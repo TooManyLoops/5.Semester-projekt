a7b8fd9a9f04f593d1ff1cce0448e7016c153a5a
frittemand <150781905+frittemand@users.noreply.github.com>
2026-05-06 12:56:57 +0200

    USER - 01 - Done

--- Vagtplanlægnings-modul/Endpoints/EmployeeEndpoints.cs
+++ Vagtplanlægnings-modul/Endpoints/EmployeeEndpoints.cs
@@ -1,4 +1,10 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.Identity.Client;
+using Microsoft.VisualBasic;
 using Vagtplanlægnings_modul.Requests;
 using Vagtplanlægnings_modul.Services;
 
@@ -10,7 +16,6 @@ public static class EmployeeEndpoints
     {
         var userEndpoint = app.MapGroup("/Employees");
         userEndpoint.MapPost("/", CreateEmployee).WithName("CreateEmployee");
-
         return app;
     }
 
@@ -19,7 +24,28 @@ public static class EmployeeEndpoints
         EmployeeRequest request
     )
     {
+        var validationResults = Validate(request);
+
+        if (validationResults.Count > 0)
+        {
+            return TypedResults.BadRequest(validationResults);
+        }
+
         var result = await service.CreateEmployee(request);
         return TypedResults.Created($"/Employees/{result.EmployeeId}", result);
+    }
+
+    public static List<ValidationResult> Validate<T>(T model)
+    {
+        var validationResults = new List<ValidationResult>();
+
+        Validator.TryValidateObject(
+            model!,
+            new ValidationContext(model!),
+            validationResults,
+            validateAllProperties: true
+        );
+
+        return validationResults;
     }
 }
